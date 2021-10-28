74835313c89c085d3b364d217e5be44df4d04349
Kris Hokr <khokr@ucdavis.edu>
2021-10-28 09:54:41 -0600

    updated with test logo

--- index.js
+++ index.js
@@ -74,39 +74,6 @@ app.get('/', (req, res) => {
 app.use(express.static('public'));
 
 
-// //middleware function that console logs request url
-// let myLogger = function(req, res, next) {
-//     console.log(req.url);
-//     //next allows next middleware function to be called
-//     next();
-// }
-
-// let requestTime = function(req, res, next) {
-//     req.requestTime = Date.now();
-//     //next allows function to be called by whichever of the two routes corresponds to the current request
-//     next();
-// }
-
-// //this designates myLogger should be used on every request below (in all app.get functions)
-// app.use(myLogger);
-// //second middleware function
-// app.use(timeStamp);
-
-
-// app.get('/', (req, res) => {
-//     let responseText = 'Welcome to my app!';
-//     //can access .requestTime method in requestTime() middleware function above
-//     responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-//     res.send(responseText);
-//   });
-  
-//   app.get('/secreturl', (req, res) => {
-//     let responseText = 'This is a secret url with super top-secret content.';
-//     responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-//     res.send(responseText);
-  
-//   });
-
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(505).send('Something broke!');
